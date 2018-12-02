43d71964bb37236a2a11a6de9e01017a37ee66c9
Mykyta Paletskyk <mukuta.paleckih@intenselab.com>
2018-12-02 10:40:43 +0200

    start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { ShopComponent } from './shop/shop.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    MenuComponent,
+    ShopComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
